ad24f99a45541e2331dfa2a2dd36f1656c6fc053
Kirill Gamazkov <kirill.gamazkov@gmail.com>
2016-01-05 02:54:33 +0300

    - Simple Swing Sample - Useful links - Basic UI components list

--- src/main/java/gumanoid/GTestRunnerApp.java
+++ src/main/java/gumanoid/GTestRunnerApp.java
@@ -1,10 +1,19 @@
 package gumanoid;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Gumanoid on 05.01.2016.
  */
 public class GTestRunnerApp {
     public static void main(String[] args) {
+        JLabel label = new JLabel("Label");
 
+        JFrame mainWindow = new JFrame("Title");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.getContentPane().add(label, BorderLayout.CENTER);
+        mainWindow.pack();
+        mainWindow.setVisible(true);
     }
 }
